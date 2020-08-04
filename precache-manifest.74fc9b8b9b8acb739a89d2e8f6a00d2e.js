63cd155e5ffc869f6488c8b5863fd2a3aae4851a
Charles Jerome Desiderio <cjdesi@gmail.com>
2020-08-04 12:32:42 -0500

    Updates

---
+++ precache-manifest.74fc9b8b9b8acb739a89d2e8f6a00d2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32eb0205e962899822bc616064e80f23",
+    "url": "/frontendmentor-job-listings/index.html"
+  },
+  {
+    "revision": "5bdd3cf5baa9c2f7d9d7",
+    "url": "/frontendmentor-job-listings/static/css/main.534988a0.chunk.css"
+  },
+  {
+    "revision": "68fa3f90de1556a9e7a5",
+    "url": "/frontendmentor-job-listings/static/js/2.a3dc07e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontendmentor-job-listings/static/js/2.a3dc07e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bdd3cf5baa9c2f7d9d7",
+    "url": "/frontendmentor-job-listings/static/js/main.61e4088d.chunk.js"
+  },
+  {
+    "revision": "4d762f40a8dab2e2da94",
+    "url": "/frontendmentor-job-listings/static/js/runtime-main.daf35c2c.js"
+  }
+]);
